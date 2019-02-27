d125f6d1fc78d306f831dcd2a0707336f608457a
codewhynot <codewhynot@gmail.com>
2019-02-27 12:47:26 +0200

    refactor cli

--- bin/commands/init.js
+++ bin/commands/init.js
@@ -1,3 +1,105 @@
-module.exports = () => {
-  console.log('is init');
+const inquirer = require('inquirer');
+const download = require('download-git-repo');
+const { resolve } = require('path');
+const currentPath = process.cwd();
+const shell = require('shelljs');
+const chalk = require("chalk");
+const { readFile, writeFile, readdir } = require('fs');
+const { emptyDir } = require('fs-extra');
+let projectName;
+
+const checkFolder = (callback) => {
+  readdir(currentPath, (err,items) => {
+    if (err) {
+      console.log(chalk.bold.red(err));
+      return;
+    } else {
+      if (items.length <= 0) {
+        callback();
+      } else {
+        console.log(chalk.bold.red('Folder is not empty'))
+        asker({
+          name: 'remove',
+          type: 'confirm',
+          message: 'Do you want to clean current folder?'
+        }, answer => {
+          if (answer.remove) {
+            emptyDir(currentPath, err => {
+              if (err) {
+                console.log(chalk.bold.red(err))
+              } else {
+                callback();
+              }
+            })
+          } else {
+            console.log(chalk.bold.green('Ok, see you!'))
+          }
+        })
+      }
+    }
+
+  });
+};
+
+const getProject = (callback) => {
+  download('codewhynot/whynotpack', currentPath, function (err) {
+    if (err) {
+      console.log(chalk.bold.red(err))
+    } else {
+      callback();
+    }
+  });
+};
+
+const changePackage = (callback) => {
+  let path = resolve(currentPath, 'package.json');
+  readFile(path, function (err,data) {
+    let result = data.toString();
+    let jsonData = JSON.parse(result);
+    jsonData.name = projectName;
+    let output = JSON.stringify(jsonData);
+    writeFile(path, output, (err) => {
+      if (err) throw err;
+    });
+    callback();
+  });
+};
+
+const asker = (questions, callback) => {
+  const prompt = inquirer.createPromptModule();
+  prompt(questions).then(data => {
+    callback(data);
+  })
+}
+
+
+module.exports = (args) => {
+    asker ({
+      name: 'name',
+      type: 'input',
+      message: 'Please enter your project name'
+    }, project => {
+      if (project.name !== '' ) {
+        projectName = project.name;
+        checkFolder ( () => {
+          getProject(() => {
+            changePackage( () => {
+              asker({
+                name: 'dependencies',
+                type: 'confirm',
+                message: 'Do uou want to install dependencies?'
+              }, answers => {
+                if (answers.dependencies) {
+                  shell.exec('npm install');
+                } else {
+                  console.log(chalk.bold.green('Ok, see you ;)'));
+                }
+              })
+            });
+          })
+        });
+      } else {
+        console.log(chalk.bold.red('please use a valid project name'));
+      }
+    });
 };
--- bin/wnp.js
+++ bin/wnp.js
@@ -1,11 +1,51 @@
 #! /usr/bin/env node
 const programm = require('commander');
+const figlet = require("figlet");
+const chalk = require("chalk");
+const commandLineUsage = require('command-line-usage');
+
+//commands
 const init = require('./commands/init');
 
-programm
-    .command('init')
-    .action(() => {
-        init();
-    });
+const commands = [
+    {
+        header: 'Command List',
+        content: [
+            { name: '{bold help}', summary: 'Display help information about Whynotpack-cli.' },
+            { name: '{bold init}', summary: 'Initializing Whynotpack in current direction' },
+        ]
+    }
+]
+const help = commandLineUsage(commands)
+
+
+if (process.argv.slice(2).length <= 0) {
+    console.log(chalk.cyan(
+        figlet.textSync("whynotpack", {
+            font: "standard",
+            horizontalLayout: "default",
+            verticalLayout: "default"
+        })
+    ));
+    console.log(chalk.cyan('\n Welcome to WHYNOTPACK CLI'));
+    console.log(chalk.cyan(help))
+} else {
+    console.log(chalk.yellow(
+        figlet.textSync("whynotpack", {
+            font: "standard",
+            horizontalLayout: "default",
+            verticalLayout: "default"
+        })
+    ));
+    console.log(chalk.green('\n Welcome to WHYNOTPACK CLI'));
+    programm
+        .command('init')
+        .action(() => {
+            init();
+        });
+}
+
 
 programm.parse(process.argv);
+
+
