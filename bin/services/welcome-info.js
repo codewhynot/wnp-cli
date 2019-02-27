12f3b2d68e0154d7b5273ffa5ca6d04c515f3e15
codewhynot <codewhynot@gmail.com>
2019-02-27 19:52:06 +0200

    remove figlet

--- bin/services/welcome-info.js
+++ bin/services/welcome-info.js
@@ -1,14 +1,10 @@
 const visual = require("chalk");
-const letters = require("figlet");
+
 
 module.exports = data => {
   console.log(
       visual.cyan(
-          letters.textSync("whynotpack", {
-              font: "Standart",
-              horizontalLayout: "default",
-              verticalLayout: "default"
-          })
+          "WHYNOTPACK"
       ),
       visual.cyan(data ? `\n\n Used command ${data} \n` : '\n\n Welcome to WHYNOTPACK-CLI \n'),
   )
