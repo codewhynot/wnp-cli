68f4e84c9c60cb077a2cef604954413e63880ef9
codewhynot <codewhynot@gmail.com>
2019-02-28 11:03:55 +0200

    Changed font

--- bin/services/welcome-info.js
+++ bin/services/welcome-info.js
@@ -2,18 +2,18 @@ const visual = require("chalk");
 const CFonts = require('cfonts');
 
 module.exports = data => {
-    CFonts.say('WHYNOTPACK!', {
+    CFonts.say('\nWNP', {
         font: 'block',
         align: 'left',
         colors: ['yellow'],
         background: 'transparent',
         letterSpacing: 1,
-        lineHeight: 1,
-        space: true,
+        lineHeight: 0.5,
+        space: false,
         maxLength: '0',
     });
 
   console.log(
-      visual.cyan(data ? ` Used command ${data} \n` : ' Welcome to WHYNOTPACK-CLI \n'),
+      visual.cyan(data ? `\n Used command ${data} \n` : '\n Welcome to WHYNOTPACK-CLI \n'),
   )
 };
